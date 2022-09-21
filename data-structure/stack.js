282740d022c43c9d412d05522cd2ddbaac4e63fe
SeungjunLee <seungjun.dev@gmail.com>
2022-09-20 23:22:17 -0700

    data structure - stack

---
+++ data-structure/stack.js
@@ -0,0 +1,12 @@
+// A  stack is a data structure has a list of elements
+// follows LIFO order (Last In, First Out)
+
+let stack = []
+
+stack.push(1) // [1]
+stack.push(2) // [2]
+
+console.log(stack);
+let p = stack.pop() // 2
+
+console.log(p)
