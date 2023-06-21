f9ef56a810b14a09679269f6a76a967b50be4161
tinodevclumsy <seungjun.dev@gmail.com>
2023-06-21 15:26:25 +0900

    optimize the solution

--- leetcode/121. Best Time to Buy and Sell Stock.js
+++ leetcode/121. Best Time to Buy and Sell Stock.js
@@ -2,21 +2,42 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function(prices) {
-    var m = 0;
-    var buyPrice = prices[0]
+var maxProfit = function (prices) {
+  var m = 0;
+  var buyPrice = prices[0];
 
-    for(var i = 1; i < prices.length; i++) {
-        var profit = prices[i] - buyPrice
+  for (var i = 1; i < prices.length; i++) {
+    var profit = prices[i] - buyPrice;
 
-        if(m < profit) {
-            m = profit
-        }
-
-        if(buyPrice > prices[i]) {
-            buyPrice = prices[i]
-        }
+    if (m < profit) {
+      m = profit;
     }
 
-    return m
- };
+    if (buyPrice > prices[i]) {
+      buyPrice = prices[i];
+    }
+  }
+
+  return m;
+};
+
+
+// optimize
+var maxProfit = function (prices) {
+  let maxProfit = 0;
+  let minPrice = prices[0];
+
+  for (let i = 1; i < prices.length; i++) {
+    const currentPrice = prices[i];
+    if (currentPrice < minPrice) {
+      minPrice = currentPrice;
+    } else {
+      const profit = currentPrice - minPrice;
+      if (profit > maxProfit) {
+        maxProfit = profit;
+      }
+    }
+  }
+
+  return maxProfit;
+};
